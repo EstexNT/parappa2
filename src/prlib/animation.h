71140c74e4d67d7b5e2d7f9c2f563a4b0f19501c
EstexNT <estex64@gmail.com>
2026-08-21 22:48:35 +0300

    prlib/prlib: match functions - PrInitializeAnimation - PrCleanupAnimation - PrSetAnimationUserData - PrGetAnimationUserData - PrGetAnimationName

--- src/prlib/animation.h
+++ src/prlib/animation.h
@@ -3,14 +3,29 @@
 
 #include "common.h"
 
+#include "objectset.h"
+
+#include "spadata.h"
+
+#define SPA_MAGIC (0x59238771)
+#define SPA_VERSION (2)
+
 class SpmNode;
-class SpaNodeAnimation;
 
 class SpaFileHeader {
 public:
-    PR_PADDING(unk0, 0x14);
+    u_int m_magic;
+    u_short m_version;
+    u_short m_flags;
+
+    PR_PADDING(unk8, 0xc);
     float unk14;
-    PR_PADDING(unk18, 0x38);
+    char m_name[32];
+    int *unk38;
+    PrLinkedList<SpaFileHeader> m_list;
+    PrObjectSet<SpaFileHeader> *m_obj_set;
+    void *m_user_data;
+    int unk4C;
     SpaNodeAnimation **unk50;
 
 public:
